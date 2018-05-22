72fd22cbd1b4bc1f19ed6dbf69b27baf371ae42b
牛加人等于朱 <niuren.zhu@icloud.com>
2018-05-22 14:12:31 +0800

    更新服务契约接口

--- ibas.approvalprocess.service/src/main/webapp/bsui/c/approvalrequest/ApprovalRequestViewView.ts
+++ ibas.approvalprocess.service/src/main/webapp/bsui/c/approvalrequest/ApprovalRequestViewView.ts
@@ -48,7 +48,7 @@ namespace approvalprocess {
                                     icon: "sap-icon://action",
                                     press: function (event: any): void {
                                         ibas.servicesManager.showServices({
-                                            proxy: new ibas.BOListServiceProxy({
+                                            proxy: new ibas.BOServiceProxy({
                                                 data: (<any>that.form.getModel()).getObject(),
                                                 converter: new bo.DataConverter(),
                                             }),
