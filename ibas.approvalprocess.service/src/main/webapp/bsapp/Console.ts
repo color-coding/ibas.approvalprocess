color-coding/ibas.documents

优化编辑及查看应用数据加载

ibas.documents.service/src/main/webapp/bsapp/Console.ts
@@ -24,6 +24,7 @@ namespace documents {
                 // 注册功能
                 this.register(new DocumentFunc());
                 // 注册服务应用
+                this.register(new DocumentEditServiceMapping());
                 this.register(new DocumentChooseServiceMapping());
                 this.register(new DocumentServiceMapping());
                 this.register(new DocumentViewServiceMapping());

color-coding/ibas.approvalprocess

ibas.approvalprocess.service/src/main/webapp/bsapp/Console.ts
@@ -28,6 +28,8 @@ namespace approvalprocess {
                 this.register(new ApprovalRequestFunc());
                 this.register(new ApprovalTemplateFunc());
                 // 注册服务应用
+                this.register(new ApprovalTemplateEditServiceMapping());
+                this.register(new ApprovalRequestEditServiceMapping());
                 this.register(new ApprovalRequestChooseServiceMapping());
                 this.register(new ApprovalRequestLinkServiceMapping());
                 this.register(new ApprovalTemplateChooseServiceMapping());